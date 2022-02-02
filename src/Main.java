978eadcac32b3deac79ca51115e45c11f0daebac
Сергей Казаков <kazakov_serg@mail.ru>
2022-02-02 17:09:10 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //Вводные данные
+        int price = 100;
+        int miles = 20;
+        //Логика
+        int bonus = price / miles;
+        System.out.println("Бонусные мили: " + bonus);
+    }
+}
